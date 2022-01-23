2bba76859c80f06bef8767f322fb42e2ba5f63dd
Thiago R Garcia <garciathiagorafael@gmail.com>
2022-01-23 18:42:48 -0300

    singleton

---
+++ src/one/digitalInnovation/test.java
@@ -0,0 +1,25 @@
+package one.digitalInnovation;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        //testes relacionados ao design pattern Singleton
+
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+    }
+}
